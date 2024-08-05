1d86564d6148898dbc3b4f02b746e91dfa2b793b
Alexey Sokolov <alexvyber@gmail.com>
2024-08-05 15:51:06 +0300

    tagged templates example

---
+++ code-creatures/ts-parrots/tagged-templates.ts
@@ -0,0 +1,33 @@
+interface Store {
+  name: string
+  type: "arrow" | "fn" | "class"
+}
+
+const store: Store = {
+  name: "ComponentName",
+  type: "arrow",
+}
+
+function tag(strings: TemplateStringsArray, ...expressions: (string | ((store: Store) => string))[]) {
+  let str = ""
+
+  for (let i = 0; i < strings.length; i++) {
+    str += strings[i]
+
+    const expression = expressions[i]
+
+    if (typeof expression === "function") {
+      str += expression(store)
+    }
+
+    if (typeof expression === "string") {
+      str += expression
+    }
+  }
+
+  return str
+}
+
+const res = tag`function ${(s) => s.name} () { return <></>}`
+
+console.log({ res })
